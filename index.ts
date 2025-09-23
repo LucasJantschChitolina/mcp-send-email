36c86892c52532e085ce034e491dc73c4b539013
Lucas Chitolina <lucas.chitolina01@gmail.com>
2025-09-23 19:41:24 -0300

    refactor: remove list audiences tool

--- index.ts
+++ index.ts
@@ -177,32 +177,6 @@ server.tool(
   },
 );
 
-server.tool(
-  'list-audiences',
-  'List all audiences from Resend. This tool is useful for getting the audience ID to help the user find the audience they want to use for other tools. If you need an audience ID, you MUST use this tool to get all available audiences and then ask the user to select the audience they want to use.',
-  {},
-  async () => {
-    console.error('Debug - Listing audiences');
-
-    const response = await resend.audiences.list();
-
-    if (response.error) {
-      throw new Error(
-        `Failed to list audiences: ${JSON.stringify(response.error)}`,
-      );
-    }
-
-    return {
-      content: [
-        {
-          type: 'text',
-          text: `Audiences found: ${JSON.stringify(response.data)}`,
-        },
-      ],
-    };
-  },
-);
-
 async function main() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
